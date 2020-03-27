0075aaa24c41846c2d5a32a1e366034e77c5ecf9
Mario Lyon <mario@digileo.com>
2020-03-27 08:43:28 +0100

    Add fetching of data to studentsService

--- frontend/src/Editable.tsx
+++ frontend/src/Editable.tsx
@@ -5,80 +5,137 @@ interface IProps {
 }
 
 interface IState {
-    columns: any,
-    data: any
+    columns: any
 }
+
+const studentsService = "http://localhost:5000"
 
 class Editable extends React.PureComponent<IProps, IState> {
     constructor(props: Readonly<{}>) {
         super(props);
         this.state = {
             columns: [
-                {title: 'Uuid', field: 'uuid'},
-                {title: 'Class', field: 'class', initialEditValue: 'initial edit value'},
-                {title: 'Name', field: 'name', initialEditValue: 'initial edit value'},
-                {
-                    title: 'Sex', field: 'sex', lookup: {'male': 'male', 'female': 'female'},
-                },
-                {title: 'Age', field: 'age', initialEditValue: 'initial edit value'},
-                {title: 'Siblings', field: 'siblings', initialEditValue: 'initial edit value'},
-                {title: 'Gpa', field: 'gpa', initialEditValue: 'initial edit value'},
-            ],
-            data: [
-                {
-                    uuid: '40d6cb1a-fe69-4c11-bdfd-9d2f896f335c',
-                    class: '3',
-                    name: "Steve Jones",
-                    sex: "male",
-                    age: '22',
-                    siblings: '1'
-                },
+                {title: 'Uuid', field: 'uuid', editable: false},
+                {title: 'Class', field: 'class', type: 'numeric'},
+                {title: 'Name', field: 'name'},
+                {title: 'Sex', field: 'sex', lookup: {'male': 'male', 'female': 'female'}},
+                {title: 'Age', field: 'age', type: 'numeric'},
+                {title: 'Siblings', field: 'siblings', type: 'numeric'},
+                {title: 'Gpa', field: 'gpa', type: 'numeric'},
             ]
         }
     }
 
+    static getStudentDataFrom = (newData: any) => {
+        return {
+            'class': newData['class'],
+            'name': newData['name'],
+            'sex': newData['sex'],
+            'age': newData['age'],
+            'siblings': newData['siblings'],
+            'gpa': newData['gpa']
+        }
+    };
+
     render() {
         return (
             <MaterialTable
-                title="Editable Preview"
+                title="Students"
                 columns={this.state.columns}
-                data={this.state.data}
+                data={query =>
+                    new Promise((resolve, reject) => {
+                        // prepare your data and then call resolve like this:
+                        const response = fetch(`${studentsService}/students`);
+                        response.then(r =>
+                            r.json()
+                        ).then(
+                            json =>
+                                resolve({
+                                        data: json,
+                                        page: 0,
+                                        totalCount: json.length
+                                    }
+                                )
+                        )
+                    })
+                }
+                options={{
+                    addRowPosition: 'last',
+                    showFirstLastPageButtons: false,
+                    search: false,
+                    sorting: false,
+                    paging: false,
+                    actionsColumnIndex: 7
+                }}
                 editable={{
-                    onRowAdd: newData =>
-                        new Promise((resolve, reject) => {
-                            setTimeout(() => {
+                    onRowAdd: newData => {
+                        return new Promise((resolve, reject) => {
+                            // prepare your data and then call resolve like this:
+                            const studentData = Editable.getStudentDataFrom(newData)
+
+                            const response = fetch(`${studentsService}/student`,
                                 {
-                                    const data = this.state.data;
-                                    data.push(newData);
-                                    this.setState({data}, () => resolve());
-                                }
-                                resolve()
-                            }, 1000)
-                        }),
-                    onRowUpdate: (newData, oldData) =>
-                        new Promise((resolve, reject) => {
-                            setTimeout(() => {
+                                    method: 'POST',
+                                    cache: 'no-cache',
+                                    headers: {
+                                        'Content-Type': 'application/json',
+                                        'mario': 'yes'
+                                    },
+                                    body: JSON.stringify(studentData)
+                                });
+                            response
+                                .then(r => r.json())
+                                .then(json => resolve({
+                                    data: json,
+                                    page: 0,
+                                    totalCount: json.length
+                                }))
+                        })
+                    },
+                    onRowUpdate: (newData: any) => {
+                        return new Promise((resolve, reject) => {
+                            // prepare your data and then call resolve like this:
+                            const uuid = newData.uuid
+                            const studentData = Editable.getStudentDataFrom(newData)
+
+                            const response = fetch(`${studentsService}/student/${uuid}`,
                                 {
-                                    const data = this.state.data;
-                                    const index = data.indexOf(oldData);
-                                    data[index] = newData;
-                                    this.setState({data}, () => resolve());
-                                }
-                                resolve()
-                            }, 1000)
-                        }),
-                    onRowDelete: oldData =>
-                        new Promise((resolve, reject) => {
-                            setTimeout(() => {
+                                    method: 'PUT',
+                                    cache: 'no-cache',
+                                    headers: {
+                                        'Content-Type': 'application/json',
+                                    },
+                                    body: JSON.stringify(studentData)
+                                });
+                            response
+                                .then(r => r.json())
+                                .then(json => resolve({
+                                    data: json,
+                                    page: 0,
+                                    totalCount: json.length
+                                }))
+                        })
+                    },
+                    onRowDelete: (oldData: any) => {
+                        return new Promise((resolve, reject) => {
+                            const uuid = oldData.uuid
+                            const response = fetch(`${studentsService}/student/${uuid}`,
                                 {
-                                    let data = this.state.data;
-                                    const index = data.indexOf(oldData);
-                                    data.splice(index, 1);
-                                    this.setState({data}, () => resolve());
-                                }
-                                resolve()
-                            }, 1000)
-                        }),
+                                    method: 'DELETE',
+                                    cache: 'no-cache',
+                                    headers: {
+                                        'Content-Type': 'application/json'
+                                    }
+                                });
+                            response
+                                .then(r => r.json())
+                                .then(json => resolve({
+                                    data: json,
+                                    page: 0,
+                                    totalCount: json.length
+                                }))
+                        })
+                    },
                 }}
             />
         )
